31103f293fbff941c9e10081d65aea735908b625
Hiroya Kubo <kubo.hiroya@gmail.com>
2014-01-13 02:19:40 +0900

    beautify code.

--- views/filedrop.js
+++ views/filedrop.js
@@ -4,51 +4,51 @@
 "use strict";
 
 $(function () {
-        var uploadFiles = function (files) {
-            // FormData オブジェクトを用意
-            var fd = new FormData();
+    var uploadFiles = function (files) {
+        // FormData オブジェクトを用意
+        var fd = new FormData();
 
-            // ファイル情報を追加する
-            for (var i = 0; i < files.length; i++) {
-                fd.append("files", files[i]);
-            }
+        // ファイル情報を追加する
+        for (var i = 0; i < files.length; i++) {
+            fd.append("files", files[i]);
+        }
 
-            // XHR で送信
-            $.ajax({
-                    url: "http://" + window.location.hostname + ":" + window.location.port + "/upload",
-                    type: "POST",
-                    data: fd,
-                    processData: false,
-                    contentType: false
-                });
-        };
+        // XHR で送信
+        $.ajax({
+            url: "http://" + window.location.hostname + ":" + window.location.port + "/upload",
+            type: "POST",
+            data: fd,
+            processData: false,
+            contentType: false
+        });
+    };
 
-        // ファイル選択フォームからの入力
-        $("#form").bind("change", function () {
-                // 選択されたファイル情報を取得
-                var files = this.files;
+    // ファイル選択フォームからの入力
+    $("#form").bind("change", function () {
+        // 選択されたファイル情報を取得
+        var files = this.files;
 
-                // アップロード処理
-                uploadFiles(files);
-            });
-
-        // ドラッグドロップからの入力
-        $("div.dropTarget").bind("drop", function (e) {
-                // ドラッグされたファイル情報を取得
-                var files = e.originalEvent.dataTransfer.files;
-
-                // アップロード処理
-                uploadFiles(files);
-                return false;
-            })
-            .bind("dragenter", function () {
-                    // false を返してデフォルトの処理を実行しないようにする
-                    $('div.dropTarget').addClass('dragEnter');
-                    return false;
-                })
-            .bind("dragover", function () {
-                    // false を返してデフォルトの処理を実行しないようにする
-                    $('div.dropTarget').addClass('dragOver');
-                    return false;
-                });
+        // アップロード処理
+        uploadFiles(files);
     });
+
+    // ドラッグドロップからの入力
+    $("div.dropTarget").bind("drop", function (e) {
+        // ドラッグされたファイル情報を取得
+        var files = e.originalEvent.dataTransfer.files;
+
+        // アップロード処理
+        uploadFiles(files);
+        return false;
+    })
+        .bind("dragenter", function () {
+            // false を返してデフォルトの処理を実行しないようにする
+            $('div.dropTarget').addClass('dragEnter');
+            return false;
+        })
+        .bind("dragover", function () {
+            // false を返してデフォルトの処理を実行しないようにする
+            $('div.dropTarget').addClass('dragOver');
+            return false;
+        });
+});
