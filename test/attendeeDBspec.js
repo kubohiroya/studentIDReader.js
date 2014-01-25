7d236cc113f62338d499b1445f87eb7b2ac97d9b
Hiroya Kubo <kubo.hiroya@gmail.com>
2014-01-25 20:20:24 +0900

    refactoring

---
+++ test/attendeeDBspec.js
@@ -0,0 +1,91 @@
+/*global require, describe, it, before, beforeEach, after, afterEach, console */
+
+var assert = require('assert');
+var fs = require('fs');
+
+require('../lib/util/dateUtil.js');
+
+var AttendeeDB = require('../lib/attendeeDB.js').AttendeeDB;
+var ReadStatusFactory = require('../lib/model.js').ReadStatusFactory;
+var ReadStatus = require('../lib/model.js').ReadStatus;
+var MemberGroups = require('../lib/grouping.js').MemberGroups;
+
+var LECTURE_ID = '00000';
+var USER_ID = 'XXXX';
+
+describe('attendeeDB', function(){
+
+    var attendeeDir = '/tmp';
+    var attendeeFilenameBase = 'hoge';
+    var numGroups = 5;
+
+    var lecture = {lectureID:LECTURE_ID};
+
+    var grouping = new MemberGroups(numGroups);
+
+    var readStatusFactory = function (attendFileEntry) {
+        if (!attendFileEntry) {
+            return undefined;
+        } else if (attendFileEntry.lectureID === lecture.lectureID) {
+            var yyyymmddhhmmss = (attendFileEntry.yyyymmdd + " " + attendFileEntry.hhmmss);
+            var datetime = yyyymmddhhmmss.split(/[\s\-\:\,]/).createDateAs(['year', 'mon', 'day', 'hour', 'min', 'sec']);
+            if (grouping && attendFileEntry.groupID) {
+                var groupIndex = parseInt(attendFileEntry.groupID) - 1;
+                grouping.addGroupMember(groupIndex, attendFileEntry.userID);
+            }
+            return new ReadStatus(attendFileEntry.lectureID, attendFileEntry.userID, datetime);
+        } else if (attendFileEntry.lectureID) {
+            console.log("[warn] lectureID missmatch:" + lecture.lectureID + " != " + attendFileEntry.lectureID);
+        }
+    };
+
+    var attendFileEntryFactory = function (readStatus, student, groupID) {
+        return {
+            yyyymmdd: readStatus.time.get_yyyymmdd(),
+            wdayatime: readStatus.time.get_wdayatime(),
+            hhmmss: readStatus.time.get_hhmmss(),
+            lectureID: readStatus.lectureID,
+            userID: readStatus.userID,
+            fullname: student ? student.fullname : undefined,
+            furigana: student ? student.furigana : undefined,
+            groupID: groupID ? groupID : undefined
+        };
+    };
+
+    var attendeeDB = new AttendeeDB(attendeeDir,
+        attendeeFilenameBase,
+        readStatusFactory,
+        attendFileEntryFactory);
+
+    if(fs.exists(attendeeDB.csvDB.filename)){
+        fs.unlinkSync(attendeeDB.csvDB.filename);
+    }
+
+    it('userIDをキーに保存(store)した値を、再度、userIDをキーに取得(get)できる', function(){
+        var now = new Date();
+        now.setTime(now.getTime() - now.getMilliseconds());
+        var readStatus = new ReadStatus(LECTURE_ID, USER_ID, now);
+
+        var student = {lectureID:LECTURE_ID, userID:USER_ID, time: now};
+
+        var groupID = 1;
+
+        attendeeDB.store(readStatus, student, groupID);
+
+        assert.deepEqual(student, attendeeDB.get(student.userID));
+    });
+
+    it('CSVファイルが保存される', function(){
+        fs.stat(attendeeDB.csvDB.filename, function(stat){
+            assert.equal(stat.isFile(), true);
+        });
+    });
+
+    it('CSVファイルの内容が正しく書き込まれる', function(){
+        fs.stat(attendeeDB.csvDB.filename, function(stat){
+            assert.equal(stat.isFile(), true);
+        });
+    });
+
+    //fs.unlinkSync(attendeeDB.csvDB.filename);
+});
--- test/groupingspec.js
+++ test/groupingspec.js
@@ -1,18 +1,42 @@
+/*global require, describe, it, before, beforeEach, after, afterEach, console */
+
 var assert = require('assert');
-require('../lib/util/arrayUtil.js'); 
+require('../lib/util/arrayUtil.js');
 var MemberGroups = require('../lib/grouping.js').MemberGroups;
 
 var dummyUserID = 0;
 
+
+function createRange(start, count){
+    var assertArray = [];
+    for (var i = 0; i < count; i++) {
+        assertArray.push(start+i);
+    }
+    return assertArray;
+}
+
+function createCount(grouping, numMinimumMembers, numGroups){
+    var count = {};
+    for (var dummyUserIDBase = 0; dummyUserIDBase < numMinimumMembers; dummyUserIDBase += numGroups ){
+        for(var dummyUserID = dummyUserIDBase; dummyUserID < dummyUserIDBase + numGroups; dummyUserID++ ){
+            var groupIndex = grouping.getGroupIndexOf(dummyUserID);
+            if(! count[groupIndex]){
+                count[groupIndex]=1;
+            }else{
+                count[groupIndex]++;
+            }
+        }
+    }
+    return count;
+}
+
+
 describe('逐次ランダムなグループ分け', function(){
-        var numUsers = 100;
-        var numGroups = 5;
+        var numUsers =  30;
+        var numGroups = 6;
         var grouping = new MemberGroups(numGroups);
 
-        var assertArray = [];
-        for (var i = 0; i < numGroups; i++) {
-            assertArray.push(i);
-        }
+        var assertArray = createRange(0, numGroups);
 
         for (var dummyUserID = 0; dummyUserID < numUsers; dummyUserID++ ){
             var groupIndex = grouping.chooseRandomCandidateGroupIndex();
@@ -26,6 +50,7 @@ describe('逐次ランダムなグループ分け', function(){
                         var groupIndex = grouping.getGroupIndexOf(dummyUserID);
                         segment.push(groupIndex);
                     }
+                    //console.log(segment);
                     assert.deepEqual(segment.sort(), assertArray.sort());
                 }
             });
@@ -34,48 +59,53 @@ describe('逐次ランダムなグループ分け', function(){
 describe('複合的なグループ分け', function(){
 
         var numUsers = 120;
-        var numGroups = 12;
+        var numGroups = 10;
         var numMinimumMembers = 60;
-        
+
         var grouping = new MemberGroups(numGroups, numMinimumMembers);
 
-        var assertArray = [];
-        for (var i = 0; i < numGroups; i++) {
-            assertArray.push(i);
-        }
+        var assertArray = createRange(0, numGroups);
 
         for (var dummyUserID = 0; dummyUserID < numUsers; dummyUserID++ ){
             grouping.chooseAndAddRandomCandidateGroupIndex(dummyUserID);
         }
 
         it('テスト条件の確認', function(){
-                assert.equal(true, numUsers % numGroups == 0);
-                assert.equal(true, numMinimumMembers % numGroups == 0);
+                assert.equal(true, numUsers % numGroups === 0);
+                assert.equal(true, numMinimumMembers % numGroups === 0);
                 assert.equal(true, numMinimumMembers <= numUsers);
             });
 
         it('ユーザのうち一定については完全にランダム(mode 0)', function(){
-                //assert mode 0
-                for (var dummyUserIDBase = 0; dummyUserIDBase < numMinimumMembers; dummyUserIDBase += numGroups ){
-                    var segment = [];
-                    for(var dummyUserID = dummyUserIDBase; dummyUserID < dummyUserIDBase + numGroups; dummyUserID++ ){
-                        var groupIndex = grouping.getGroupIndexOf(dummyUserID);
-                        segment.push(groupIndex);
-                    }
-                    assert.notDeepEqual(segment.sort(), assertArray.sort()); // in a certain case, this test may fail.
-                }
-            });
+            //assert mode 0
+            var count0 = createCount(grouping, numMinimumMembers, numGroups);
+
+            for(var groupIndex = 0; groupIndex < numGroups; groupIndex++){
+                assert.equal(count0[groupIndex], numMinimumMembers / numGroups);
+            }
+        });
 
         it('追加分についてはグループ数ごとの逐次ランダムグなループ分け(mode 1)', function(){
-                //assert mode 1
-                for (var dummyUserIDBase = numMinimumMembers; dummyUserIDBase < numUsers; dummyUserIDBase += numGroups ){
-                    var segment = [];
-                    for(var dummyUserID = dummyUserIDBase; dummyUserID < dummyUserIDBase + numGroups; dummyUserID++ ){
-                        var groupIndex = grouping.getGroupIndexOf(dummyUserID);
-                        segment.push(groupIndex);
+            //assert mode 1
+            var count1 = {};
+
+            for (var dummyUserIDBase = numMinimumMembers; dummyUserIDBase < numUsers; dummyUserIDBase += numGroups ){
+                var segment = [];
+                for(var dummyUserID = dummyUserIDBase; dummyUserID < dummyUserIDBase + numGroups; dummyUserID++ ){
+                    var selectedGroupIndex = grouping.getGroupIndexOf(dummyUserID);
+                    if(! count1[selectedGroupIndex]){
+                        count1[selectedGroupIndex]=1;
+                    }else{
+                        count1[selectedGroupIndex]++;
                     }
-                    assert.deepEqual(segment.sort(), assertArray.sort());
+                    segment.push(selectedGroupIndex);
                 }
-            });
+                assert.deepEqual(segment.sort(), assertArray.sort());
+            }
+
+            for(var groupIndex = 0; groupIndex < numGroups; groupIndex++){
+                assert.equal(count1[groupIndex], (numUsers - numMinimumMembers) / numGroups);
+            }
+        });
 
     });
